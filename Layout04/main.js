829876913c0e87a08c205b30f48c67eb885a9e32
tdudkowski <tdudkowski@gmail.com>
2019-02-06 19:21:23 +0100

    sublevels added

--- Layout04/main.js
+++ Layout04/main.js
@@ -10,82 +10,130 @@ const articles = document.querySelectorAll('main article');
 const linksArray = [...links];
 const artArray = [...articles];
 const picArray = ['77', '73', '59'];
+
+const subcounter = document.querySelector('span.subinfo');
+const btnsubup = document.getElementById('subup');
+const btnsubprev = document.getElementById('subprev');
+const btnsubnext = document.getElementById('subnext');
+
+let subNumber = document.querySelector('main').dataset.key;
 let j = 0;
 flag = false;
 
+// SUBNAV
+const submenu = document.querySelectorAll('.submenu li');
+const submenuArray = [...submenu];
+
+const submenuGo = function () {
+  console.log(this, j);
+  window.open(`index${j+1}.html`, '_self');
+}
+
+for (let i = 0; i < submenuArray.length; i++) {
+  submenuArray[i].addEventListener('click', submenuGo);
+}
+
 const init = () => {
- for (let i = 0; i < artArray.length; i++) {
-  artArray[i].style.display = 'none';
- }
- intro.style.display = 'flex';
- counter.textContent = '';
- linksArray.forEach(link => link.classList.add('disabled'));
- picture.style.backgroundImage = `url(http://picsum.photos/300/1000?image=1053)`;
- flag = true;
- for (let i = 0; i < linksArray.length; i++) {
-  linksArray[i].addEventListener('click', function () {
-   j = i;
-   schuffle();
-   flag = false;
-  });
- }
+  if (flag) {
+    window.open('index.html', '_self');
+    return;
+  }
+  for (let i = 0; i < artArray.length; i++) {
+    artArray[i].style.display = 'none';
+  }
+  intro.style.display = 'flex';
+  if (counter) {
+    counter.textContent = ``;
+  }
+  if (subcounter) {
+    subcounter.textContent = `${subNumber}._`;
+  }
+  linksArray.forEach(link => link.classList.add('disabled'));
+  picture.style.backgroundImage = `url(http://picsum.photos/300/1000?image=1053)`;
+  flag = true;
+  for (let i = 0; i < linksArray.length; i++) {
+    linksArray[i].addEventListener('click', function () {
+      j = i;
+      schuffle();
+      flag = false;
+    });
+  }
 }
 
 init();
 
 const schuffle = () => {
- for (let i = 0; i < artArray.length; i++) {
-  artArray[i].style.display = 'none';
- }
- intro.style.display = 'none';
- artArray[j].style.display = 'flex';
- counter.textContent = j + 1;
- picture.style.backgroundImage = `url(http://picsum.photos/300/1000?image=10${picArray[j]})`;
+  for (let i = 0; i < artArray.length; i++) {
+    artArray[i].style.display = 'none';
+  }
+  intro.style.display = 'none';
+  artArray[j].style.display = 'flex';
+  if (counter) {
+    counter.textContent = `${j + 1}.`;
+  }
+  if (subcounter) {
+    subcounter.textContent = `${subNumber}.${j + 1}`;
+  }
+  picture.style.backgroundImage = `url(http://picsum.photos/300/1000?image=10${picArray[j]})`;
 }
 
 const prevChange = () => {
- j--;
- if (j < 0) {
-  j = artArray.length - 1
- }
- if (flag) {
-  j = artArray.length - 1;
-  flag = false;
- }
- schuffle();
+  j--;
+  if (j < 0) {
+    j = artArray.length - 1
+  }
+  if (flag) {
+    j = artArray.length - 1;
+    flag = false;
+  }
+  schuffle();
 }
 
 const nextChange = () => {
- j++;
- if (j == artArray.length) {
-  j = 0
- }
- if (flag) {
-  j = 0;
-  flag = false;
- }
- schuffle();
+  j++;
+  if (j == artArray.length) {
+    j = 0
+  }
+  if (flag) {
+    j = 0;
+    flag = false;
+  }
+  schuffle();
 }
 
 // CHANGING SLIDES WHEN KBS IS CLICKED
 const keyChangeSlide = (e) => {
- //left keyCode 37, right keyCode 39
- switch (e.keyCode) {
-  case 37:
-   prevChange();
-   break;
-  case 38:
-   init();
-   break;
-  case 39:
-   nextChange();
-   break;
-  default:
-   console.log(e.keyCode);
- }
+  if (e.keyCode == 37) {
+    prevChange();
+  }
+  if (e.keyCode == 38) {
+    if (document.querySelector('main').dataset.key != 0) {
+      if (subcounter) {
+        subcounter.textContent = `${subNumber}._`;
+      }
+      init();
+    }
+  }
+  if (e.keyCode == 39) {
+    nextChange();
+  }
+  if (e.keyCode == 40) {
+    if (document.querySelector('div.intro').style.display == 'none') {
+      submenuGo();
+    }
+  }
+  console.log(j);
 }
 
-btnprev.addEventListener('click', prevChange);
-btnnext.addEventListener('click', nextChange);
 window.addEventListener('keydown', keyChangeSlide);
-btnup.addEventListener('click', init);
+if (btnprev) {
+  btnprev.addEventListener('click', prevChange);
+  btnnext.addEventListener('click', nextChange);
+  btnup.addEventListener('click', init);
+}
+
+if (btnsubup) {
+  btnsubup.addEventListener('click', init);
+  btnsubprev.addEventListener('click', prevChange);
+  btnsubnext.addEventListener('click', nextChange);
+}
