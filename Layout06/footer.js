efd6c6a9e529a11c8bdf8ce6b64661eb738c57bd
tdudkowski <tdudkowski@gmail.com>
2020-05-30 07:29:38 +0200

    update content

---
+++ Layout06/footer.js
@@ -0,0 +1,18 @@
+<footer>
+  <nav>
+    <ul>
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">Stuff</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+    </ul>
+  </nav>
+</footer>;
